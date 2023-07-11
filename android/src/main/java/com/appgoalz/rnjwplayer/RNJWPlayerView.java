548a344eb0770e9de46fe2343b6e9161f0137252
leinnus <leinnus@gmail.com>
2023-07-12 01:16:31 +0900

    Add files via upload

--- android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerView.java
+++ android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerView.java
@@ -94,6 +94,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RNJWPlayerView extends RelativeLayout implements
         VideoPlayerEvents.OnFullscreenListener,
